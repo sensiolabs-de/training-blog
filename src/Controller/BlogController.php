82b1dea3ba8a1e9485102d17044bacae4d1e349e
Christopher Hertel <christopher.hertel@sensiolabs.de>
2018-10-24 12:13:00 +0200

    blog front page

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="blog_")
+ */
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+}
